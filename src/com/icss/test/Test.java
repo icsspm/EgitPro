3f97ad182a2099324866512c1a4c5143fd8b4a55
gitacc_pm <gitacc_pm@163.com>
2020-02-11 16:00:37 +0800

    hha

--- src/com/icss/test/Test.java
+++ src/com/icss/test/Test.java
@@ -1,6 +1,9 @@
 package com.icss.test;
 
-public class Test {
-  System.out.println("123");
+public class Test {	
+	public static void main(String[] args) {
+		System.out.println("123");
+	}
+  
 
 }
